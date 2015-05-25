602f5efb08aa347c792e66a9e3b2ac7b2c7dc6c7
Jake Craige <james.craige@gmail.com>
2015-05-25 12:48:53 -0700

    Initial Commit

---
+++ YardClub/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  YardClub
+//
+//  Created by James Craige on 5/25/15
+//  Copyright (c) 2015 thoughtbot. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
